6abf4f0a370c5bb6b96182a50e2c537beec9f282
Asmod4n <Asmod4n@users.noreply.github.com>
2021-06-14 17:41:18 +0200

    don't check for leaks

--- build_config.rb
+++ build_config.rb
@@ -1,8 +1,8 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
   enable_debug
-  conf.enable_sanitizer "address,undefined,leak"
-  conf.cc.flags << '-fno-omit-frame-pointer' << '-g' << '-ggdb'
+  conf.enable_sanitizer "address,undefined"
+  conf.cc.flags << '-fno-omit-frame-pointer'
   conf.enable_debug
   conf.enable_test
   conf.gembox 'full-core'
