464d31f3dd7c3dc34ad387fb4302218fbcd7f892
Asmod4n <791770+Asmod4n@users.noreply.github.com>
2025-08-17 12:03:32 +0200

    test with sanitizers

--- build_config.rb
+++ build_config.rb
@@ -1,9 +1,9 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
-  #enable_debug
-  #conf.enable_sanitizer "address,undefined"
+  enable_debug
+  conf.enable_sanitizer "address,undefined"
   conf.cc.flags << '-fno-omit-frame-pointer'
-  #conf.enable_debug
+  conf.enable_debug
   conf.enable_test
   conf.gembox 'full-core'
   conf.gem File.expand_path(File.dirname(__FILE__))
